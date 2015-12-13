44a362c43969b13fd5f075efcc87faf526ab3dd4
Grímur Daníelsson <grimur.danielsson@gmail.com>
2015-12-13 15:39:07 +0000

    Lagfæra acceptance tests.

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -8,16 +8,17 @@ var acceptanceUrl = process.env.ACCEPTANCE_URL;
 describe('TEST ENV GET /api/gameHistory', function () {
 
   it('Should have ACCEPTANCE_URL environment variable exported.', function () {
-    acceptanceUrl.should.be.ok();
+    /*jshint -W030 */
+    acceptanceUrl.should.be.ok;
   });
 
   it('should execute same test using old style', function (done) {
 
-    var command =     {
-      id : "1234",
-      gameId : "999",
+    var command = {
+      id: "1234",
+      gameId: "100000",
       comm: "CreateGame",
-      user: "Gulli",
+      userName: "Gulli",
       name: "TheFirstGame",
       timeStamp: "2014-12-02T11:29:29"
     };
@@ -30,7 +31,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
       .end(function (err, res) {
         if (err) return done(err);
         request(acceptanceUrl)
-          .get('/api/gameHistory/999')
+          .get('/api/gameHistory/100000')
           .expect(200)
           .expect('Content-Type', /json/)
           .end(function (err, res) {
@@ -39,11 +40,10 @@ describe('TEST ENV GET /api/gameHistory', function () {
             should(res.body).eql(
               [{
                 "id": "1234",
-                "gameId": "999",
-                "event": {
-                  "type": "GameCreated",
-                  "user": "Gulli",
-                },
+                "gameId": "100000",
+                "event": "GameCreated",
+                "userName": "Gulli",
+                "name": "TheFirstGame",
                 "timeStamp": "2014-12-02T11:29:29"
               }]);
             done();
